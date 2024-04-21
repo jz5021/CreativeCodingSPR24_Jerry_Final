8f25bec565e7e30436dff857b08f2540fa45b124
jz5021 <157434904+jz5021@users.noreply.github.com>
2024-04-21 01:12:21 -0400

    Sprite
    Checking if the main sprite works and shows up properly like I want it to

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 //Jerry Zhao_Final Project
 
+let sprite;
+
+function preload(){
+    sprite = loadImage('assets/SpriteV1.png');
+}
+
 function setup(){
     createCanvas(1000,1000);
 }
 
 function draw(){
-
+    background(255);
+    image(sprite,0,0, height,width);
 }
