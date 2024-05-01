c4bd9ffd788d43579601a393999b013f0f3bd7cd
jz5021 <157434904+jz5021@users.noreply.github.com>
2024-05-01 10:11:07 -0400

    Shop Button_v1
    Working on button to make reactive to mouse position and whether it's in the bounds

--- sketch.js
+++ sketch.js
@@ -1,16 +1,59 @@
 //Jerry Zhao_Final Project
 
-let sprite;
+let shop;
+let eyeArray = [];
 
-function preload(){
-    sprite = loadImage('assets/SpriteV1.png');
-}
+/*function preload(){
+    shop = loadImage('');
+    eye = loadImage('');
+}*/
 
 function setup(){
-    createCanvas(1000,1000);
+    createCanvas(1920,1080);
+
+    shop = new shopButton(0, 0, 400, 200);
 }
 
 function draw(){
-    background(255);
-    image(sprite,0,0, height,width);
+    background(0);
+    shop.display();
+    shop.update();
+}
+
+class shopButton{
+    constructor(xPos, yPos, width, height){
+        this.x = xPos;
+        this.y = yPos;
+        this.w = width;
+        this.h = height;
+        this.mouseOver = false; //tracks whether or not the mouse is within the bounds of the box
+    }
+
+    display(){
+        rectMode(CENTER);
+        
+        if(this.mouseOver){
+            fill(150);
+        } else {
+            fill(255);
+        }
+        
+        push();
+        translate(width/2, height/2);
+        rect(this.x, this.y, this.w, this.h);
+        pop();
+    }
+
+    update(){
+        push();
+        translate(width/2, height/2);
+        if (mouseX > -this.w / 2 && mouseX < this.w / 2 &&
+            mouseY > -this.h / 2 && mouseY < this.h / 2){
+            this.mouseOver = true;
+        } else {
+            this.mouseOver = false;
+        }
+        pop();
+    }
+
 }
